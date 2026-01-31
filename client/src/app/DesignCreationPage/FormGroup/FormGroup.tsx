a07ce7c14072b1f4a1352490ade9c469afd0a193
HagbardCeline <janpauldahlke@gmail.com>
2026-01-31 21:32:56 +0100

    comment about desired values

---
+++ client/src/app/DesignCreationPage/FormGroup/FormGroup.tsx
@@ -0,0 +1,14 @@
+/*
+
+* style : pixelate | triangle | hexagaon
+* color: how may colors 2-10
+* granularity: how many parts per inch 10-100 // how large or small is an individual style
+*/
+
+export const FormGroup = () => {
+  return (
+    <div>
+      <h1>Form Group</h1>
+    </div>
+  );
+};
